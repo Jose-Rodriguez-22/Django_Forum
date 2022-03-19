aa840920ff1940c458de77a521d169d09e41203e
Jose Rodriguez <jrox9910@gmail.com>
2022-03-19 01:37:00 -0500

    modified files

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+
+
+def index(request):
+    # if the method is POST
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        # if the form is valid
+        if form.is_valid():
+            #Yes, Save
+            form.save()
+
+            # Redirect to home
+            return HttpResponseRedirect('/')
+
+        else:
+            # No, show error
+            return HttpResponseRedirect(form.errors.as_json())
+
+    # Get all posts, limit = 20
+    posts = Post.objects.all().order_by('-created_atd')[:20]
+
+    # show
+    return render(request, 'posts.html',
+                  {'posts': posts})
+
+
+def delete(request, post_id):
+    # Find post
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
